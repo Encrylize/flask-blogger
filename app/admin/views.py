ff04c035329781c8cd40adaf1113f3dfa8def368
Encrylize <encrylize@gmail.com>
2015-12-18 16:34:39 +0100

    Add admin post display

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, current_app
 from flask_security import current_user
+
+from app.models import Post
 
 admin = Blueprint('admin', __name__)
 
@@ -8,6 +10,13 @@ admin = Blueprint('admin', __name__)
 @admin.route('/index')
 def index():
     return render_template('admin/index.html', title='Admin')
+
+
+@admin.route('/posts')
+@admin.route('/posts/<int:page>')
+def show_posts(page=1):
+    posts = Post.query.paginate(page, current_app.config.get('POSTS_PER_PAGE', 20))
+    return render_template('admin/posts.html', posts=posts)
 
 
 @admin.before_request
