7801c5d7430233eb78ab8b2a91f5960bd808b2c7
Encrylize <encrylize@gmail.com>
2015-12-18 16:13:56 +0100

    Move admin authentication into before_request handler

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,11 +1,16 @@
-from flask import Blueprint, render_template
-from flask_security import login_required
+from flask import Blueprint, render_template, redirect, url_for
+from flask_security import current_user
 
 admin = Blueprint('admin', __name__)
 
 
 @admin.route('/')
 @admin.route('/index')
-@login_required
 def index():
     return render_template('admin/index.html', title='Admin')
+
+
+@admin.before_request
+def require_login():
+    if not current_user.is_authenticated:
+        return redirect(url_for('security.login', next='admin'))
