b70819631b719d6c2984fdc07e3185b3f4f9b8bd
Encrylize <encrylize@gmail.com>
2015-12-20 18:14:54 +0100

    Add post creation view

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, current_app
 from flask_security import current_user
 
 from app.models import Post
+from app.admin.forms import PostForm
 
 admin = Blueprint('admin', __name__)
 
@@ -17,6 +18,16 @@ def index():
 def show_posts(page=1):
     posts = Post.query.paginate(page, current_app.config.get('POSTS_PER_PAGE', 20))
     return render_template('admin/posts.html', posts=posts)
+
+
+@admin.route('/new/post', methods=['GET', 'POST'])
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        form.save()
+        return form.redirect(url_for('admin.index'))
+
+    return render_template('admin/post_form.html', form=form)
 
 
 @admin.before_request
