253a407161a0b88d4530bfb70366063e9f2d6d7f
Encrylize <encrylize@gmail.com>
2015-12-21 14:40:39 +0100

    Fix error on PostForm initialization with Post object

--- app/utils/fields.py
+++ app/utils/fields.py
@@ -14,12 +14,22 @@ class TagListField(Field):
 
     def _value(self):
         if self.data:
-            return u', '.join(self.data)
+            try:
+                # The data is a list of strings
+                return u', '.join(self.data)
+            except TypeError:
+                # The data is a list of Tag objects
+                return u', '.join([tag.name for tag in self.data])
         else:
             return u''
 
     def process_formdata(self, valuelist):
         if valuelist:
-            self.data = [x.strip() for x in valuelist[0].split(',') if x]
+            try:
+                # The data is a string
+                self.data = [x.strip() for x in valuelist[0].split(',') if x]
+            except AttributeError:
+                # The data is a list of Tag objects
+                self.data = [tag.name for tag in valuelist[0]]
         else:
             self.data = []
