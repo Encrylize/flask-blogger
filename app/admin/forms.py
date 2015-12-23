767cf250a23c164cfcf7d6eba5a48116e3b111e5
Encrylize <encrylize@gmail.com>
2015-12-23 15:49:25 +0100

    Return Post object in PostForm save method

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -19,12 +19,20 @@ class PostForm(RedirectForm):
         self.obj = kwargs.get('obj')
 
     def save(self):
+        """
+        Saves the Post object.
+
+        Returns:
+            The Post object
+
+        """
+
         if not self.obj:
             self.obj = Post()
 
         self.populate_obj(self.obj)
         self.obj.tags = [get_or_create(Tag, name=tag)[0] for tag in self.tags.data]
-        self.obj.save()
+        return self.obj.save()
 
     def populate_obj(self, obj):
         for name, field in self._fields.items():
